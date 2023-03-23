b2611f6f0e9c8d556b7af7f061d33754ad4230c3
Supragya2912 <anandsupragya@gmail.com>
2023-03-24 02:54:17 +0530

    Added the Certifications

--- src/components/Certifications/Certifications.jsx
+++ src/components/Certifications/Certifications.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import "./Certifications.css";
+import { Data } from "./Data";
 
 const Certifications = () => {
   return (
-    <div>Certifications</div>
+    <section className="certificate container section">
+      <h2 className="section__title">Certification</h2>
+      <span className="section__subtitle">My Certificates</span>
+
+      <div className="certificate__container">
+       {
+        Data.map(({id , image ,title , description}) => {
+          return (
+            <div className="certificate__content" key={id}>
+
+              <img src={image} alt="" className="certificate__image" />
+              <h3 className="certificate__title">{title}</h3>
+              <p className="certificate__description">{description}</p>
+
+              </div>
+          )
+        })
+       }
+      </div>
+    </section>
   )
 }
 
---
+++ src/components/Certifications/Data.jsx
@@ -0,0 +1,50 @@
+import Intro_to_Frontend from "../../assets/Intro_to_Frontend.png";
+import javascript_basic from "../../assets/javascript_basic.png";
+import Prog_with_JS from "../../assets/Prog_with_JS.png";
+import React_Basics from "../../assets/React_Basics.png";
+import Version_Control from "../../assets/Version_Control.png";
+import Internship_Certificate from "../../assets/Internship_Certificate.png";
+
+
+export const Data = [
+
+    {
+        id: 0,
+        image: Internship_Certificate,
+        title: "Full Stack Developer Internship",
+        description:
+        "I received this certificate on completion of two month of internship at Phyfarm. I worked as a Front-End Developer and built user responsive apps using React JS."
+    },
+    {
+        id: 1,
+        image: Intro_to_Frontend,
+        title: "Introduction to Frontend Development",
+        description:
+        "This certificate from Coursera with the title Introduction to Frontend Development represents a valuable achievement in the field of web development. Through this course, I have gained foundational knowledge and practical skills in HTML, CSS."
+
+    },
+    {
+        id: 2,
+        image: javascript_basic,
+        title: "JavaScript Basic",
+        description: "This is a certificate which I received on completing a test of Javascript on Hackerrank."
+    },
+    {
+        id: 3,
+        image: React_Basics,
+        title: "React Basics",
+        description: "This is a React JS certification which I received on completion of project on Coursera."
+    },
+    {
+        id: 4,
+        image: Prog_with_JS,
+        title: "Programming with JavaScript",
+        description: "This is the JavaScript course certification which was issued by Coursera."
+    },
+    {
+        id: 5,
+        image: Version_Control,
+        title: "Version Control",
+        description: "This is a version control certification regarding the concepts of git and github."
+    }
+]
